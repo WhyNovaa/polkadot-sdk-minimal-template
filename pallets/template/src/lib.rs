6cac955be826898c049fd33062afeac81b1664dc
Novikau Arseniy <novikov.arsenij@gmail.com>
2025-02-05 16:23:53 +0300

    delete useless code

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -33,14 +33,6 @@ pub mod my_pallet {
 	};
 	use polkadot_sdk::sp_runtime::transaction_validity::InvalidTransaction;
 
-	/*pub type Balance = u128;
-
-	#[pallet::storage]
-	pub type TotalInsurance<T: Config> = StorageValue<_, Balance>;
-
-	#[pallet::storage]
-	pub type Balances<T: Config> = StorageMap<_, _, T::AccountId, Balance>;*/
-
 	#[pallet::storage]
 	pub type LastRequests<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, (BalanceOf<T>, BlockNumberFor<T>), ValueQuery>;
 
@@ -77,39 +69,6 @@ pub mod my_pallet {
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/*#[pallet::call_index(0)]
-		pub fn mint_unsafe(
-			origin: T::RuntimeOrigin,
-			dest: T::AccountId,
-			amount: Balance,
-		) -> DispatchResult {
-			let _anyone = ensure_signed(origin)?;
-
-			Balances::<T>::mutate(dest, |b| *b = Some(b.unwrap_or(0) + amount));
-
-			TotalInsurance::<T>::mutate(|t| *t = Some(t.unwrap_or(0) + amount));
-			Ok(())
-		}
-
-		#[pallet::call_index(1)]
-		pub fn transfer(
-			origin: T::RuntimeOrigin,
-			dest: T::AccountId,
-			amount: Balance,
-		) -> DispatchResult {
-			let sender = ensure_signed(origin)?;
-			let sender_balance = Balances::<T>::get(&sender).ok_or("NonExistentAccount")?;
-
-			sender_balance.checked_sub(amount).ok_or("InsufficientBalance")?;
-			
-			let reminder = sender_balance - amount;
-
-			Balances::<T>::mutate(dest, |b| *b = Some(b.unwrap_or(0) + amount));
-			Balances::<T>::insert(sender, reminder);
-
-			Ok(())
-		}*/
-
 		#[pallet::call_index(2)]
 		pub fn token_faucet(
 			origin: T::RuntimeOrigin,
@@ -196,77 +155,3 @@ pub mod my_pallet {
 
 	}
 }
-
-
-/*#[cfg(test)]
-pub mod runtime {
-	use super::*;
-	use polkadot_sdk::{
-		frame_system,
-		pallet_balances,
-		frame_support::{derive_impl, parameter_types},
-		xcm_emulator::{BlockNumberFor, Test}
-	};
-	use frame::runtime::prelude::construct_runtime;
-
-	use my_pallet as pallet_currency;
-
-	pub const BLOCKS_PER_HOUR: BlockNumberFor<Runtime> = 60 * 60 / 6;
-
-	construct_runtime!(
-		pub enum Runtime {
-			System: frame_system,
-			Currency: pallet_currency,
-		}
-	);
-
-	parameter_types! {
-    	pub const AccumulationPeriod: BlockNumberFor<Runtime> = BLOCKS_PER_HOUR * 24;
-    	pub const FaucetAmount: Balance = 1000;
-		pub const FaucetPalletId: PalletId = PalletId(*b"pa/facet");
-	}
-
-	type Block = frame_system::mocking::MockBlock<Runtime>;
-
-	#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
-	impl frame_system::Config for Runtime {
-		type Block = Block;
-		type AccountId = u64;
-	}
-
-	impl pallet_currency::Config for Runtime {
-		type Currency = pallet_balances::Pallet<Runtime>;
-		type AccumulationPeriod = AccumulationPeriod;
-		type FaucetAmount = FaucetAmount;
-		type PalletId = PalletId;
-	}
-}
-
-#[cfg(test)]
-mod test {
-	use super::*;
-
-	use polkadot_sdk::frame_support::assert_ok;
-	use frame::testing_prelude::*;
-
-	use crate::runtime::{Runtime, RuntimeOrigin};
-
-	#[test]
-	fn first_test() {
-		TestState::new_empty().execute_with(|| {
-			let account = 1_u64;
-
-			assert_eq!(Balances::<Runtime>::get(&account), None);
-			assert_eq!(TotalInsurance::<Runtime>::get(), None);
-
-			assert_ok!(Pallet::<Runtime>::mint_unsafe(
-				RuntimeOrigin::signed(account),
-				account,
-				100
-			));
-
-			assert_eq!(Balances::<Runtime>::get(&account), Some(100));
-			assert_eq!(TotalInsurance::<Runtime>::get(), Some(100));
-		});
-	}
-}*/
