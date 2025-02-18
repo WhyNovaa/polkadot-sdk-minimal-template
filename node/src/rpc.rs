bda948da3fb2f2e1f06beedace8d9bbe7159921e
Arseni Novikau <novikov.arsenij@gmail.com>
2025-02-18 18:16:41 +0300

    create get_name rpc method(just return 32 rn)

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -22,45 +22,83 @@
 
 #![warn(missing_docs)]
 
+use jsonrpsee::core::RpcResult;
+use jsonrpsee::proc_macros::rpc;
 use jsonrpsee::RpcModule;
 use minimal_template_runtime::interface::{AccountId, Nonce, OpaqueBlock};
+use minimal_template_runtime::NodeNameApi;
+use polkadot_sdk::polkadot_service::BlockT;
 use polkadot_sdk::{
-	sc_transaction_pool_api::TransactionPool,
-	sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata},
-	*,
+    sc_transaction_pool_api::TransactionPool,
+    sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata},
+    *,
 };
+use sp_api::ProvideRuntimeApi;
 use std::sync::Arc;
+
+#[rpc(client, server)]
+pub trait NodeNameApi<Block> {
+    #[method(name = "node_name")]
+    fn get_name(&self) -> RpcResult<u32>;
+}
+
+pub struct NodeName<C, P> {
+    client: Arc<C>,
+    _marker: std::marker::PhantomData<P>,
+}
+
+impl<C, P> NodeName<C, P> {
+    pub fn new(client: Arc<C>) -> Self {
+        Self {
+            client,
+            _marker: Default::default(),
+        }
+    }
+}
+
+impl<C, Block> NodeNameApiServer<<Block as BlockT>::Hash> for NodeName<C, Block>
+where
+    Block: BlockT,
+    C: ProvideRuntimeApi<Block> + HeaderBackend<Block> + Send + Sync + 'static,
+    C::Api: NodeNameApi<Block>,
+{
+    fn get_name(&self) -> RpcResult<u32> {
+        Ok(32)
+    }
+}
 
 /// Full client dependencies.
 pub struct FullDeps<C, P> {
-	/// The client instance to use.
-	pub client: Arc<C>,
-	/// Transaction pool instance.
-	pub pool: Arc<P>,
+    /// The client instance to use.
+    pub client: Arc<C>,
+    /// Transaction pool instance.
+    pub pool: Arc<P>,
 }
 
 #[docify::export]
 /// Instantiate all full RPC extensions.
 pub fn create_full<C, P>(
-	deps: FullDeps<C, P>,
+    deps: FullDeps<C, P>,
 ) -> Result<RpcModule<()>, Box<dyn std::error::Error + Send + Sync>>
 where
-	C: Send
-		+ Sync
-		+ 'static
-		+ sp_api::ProvideRuntimeApi<OpaqueBlock>
-		+ HeaderBackend<OpaqueBlock>
-		+ HeaderMetadata<OpaqueBlock, Error = BlockChainError>
-		+ 'static,
-	C::Api: sp_block_builder::BlockBuilder<OpaqueBlock>,
-	C::Api: substrate_frame_rpc_system::AccountNonceApi<OpaqueBlock, AccountId, Nonce>,
-	P: TransactionPool + 'static,
+    C: Send
+        + Sync
+        + 'static
+        + sp_api::ProvideRuntimeApi<OpaqueBlock>
+        + HeaderBackend<OpaqueBlock>
+        + HeaderMetadata<OpaqueBlock, Error = BlockChainError>
+        + 'static,
+    C::Api: sp_block_builder::BlockBuilder<OpaqueBlock>,
+    C::Api: substrate_frame_rpc_system::AccountNonceApi<OpaqueBlock, AccountId, Nonce>,
+    C::Api: NodeNameApi<OpaqueBlock>,
+    P: TransactionPool + 'static,
 {
-	use polkadot_sdk::substrate_frame_rpc_system::{System, SystemApiServer};
-	let mut module = RpcModule::new(());
-	let FullDeps { client, pool } = deps;
+    use polkadot_sdk::substrate_frame_rpc_system::{System, SystemApiServer};
+    let mut module = RpcModule::new(());
+    let FullDeps { client, pool } = deps;
 
-	module.merge(System::new(client.clone(), pool.clone()).into_rpc())?;
+    module.merge(System::new(client.clone(), pool.clone()).into_rpc())?;
 
-	Ok(module)
+    module.merge(NodeName::new(client.clone()).into_rpc())?;
+    Ok(module)
 }
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -249,7 +249,17 @@ type Header = HeaderFor<Runtime>;
 type RuntimeExecutive =
     Executive<Runtime, Block, frame_system::ChainContext<Runtime>, Runtime, AllPalletsWithSystem>;
 
-impl_runtime_apis! {
+sp_api::decl_runtime_apis! {
+    pub trait NodeNameApi {
+        fn get_name() -> u32;
+    }
+}
+sp_api::impl_runtime_apis! {
+    impl self::NodeNameApi<Block> for Runtime {
+        fn get_name() -> u32 {
+            32
+        }
+    }
     impl apis::Core<Block> for Runtime {
         fn version() -> RuntimeVersion {
             VERSION
